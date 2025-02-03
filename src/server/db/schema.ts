9c732d8966470f38d9370e99d7c0c18adf334d99
Mohamed Adel <medometwaley@gmail.com>
2025-02-03 13:37:44 +0200

    better names for schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,7 +11,7 @@ export const createTable = singlestoreTableCreator(
     (name) => `drive-tutorial_${name}`
 );
 
-export const files = createTable("files_table", {
+export const files_table = createTable("files_table", {
     id: int("id").primaryKey().autoincrement(),
     name: text("name").notNull(),
     size: int("size").notNull(),
@@ -23,7 +23,7 @@ export const files = createTable("files_table", {
     ]
 })
 
-export const folders = createTable("folders_table", {
+export const folders_table = createTable("folders_table", {
     id: int("id").primaryKey().autoincrement(),
     name: text("name").notNull(),
     parent: int("parent"),
